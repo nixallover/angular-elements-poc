333587b91dfb3bbb235824ff3cd9cdc591676b0b
Nicole Oliver <nicole@nrwl.io>
2021-05-19 21:10:19 -0700

    generated chart2 project

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/angular', '<rootDir>/libs/chart'],
+  projects: [
+    '<rootDir>/apps/angular',
+    '<rootDir>/libs/chart',
+    '<rootDir>/libs/chart2',
+  ],
 };
---
+++ libs/chart2/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'poc-root',
+  template: `
+    <h1>{{ title }}</h1>
+  `,
+
+  styles: [
+    `
+      h1 {
+        color: green;
+      }
+    `
+  ]
+})
+export class AppComponent {
+  title = 'POC Custom Element';
+}
