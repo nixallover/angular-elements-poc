5c28e8ddfdcaeab45ee161140fc46c5196c4c4a0
Nicole Oliver <nicole@nrwl.io>
2021-05-19 21:33:47 -0700

    Add angular elements code

--- libs/chart2/src/app/app.component.ts
+++ libs/chart2/src/app/app.component.ts
@@ -2,17 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'poc-root',
-  template: `
-    <h1>{{ title }}</h1>
-  `,
+  template: ` <h1>{{ title }}</h1> `,
 
   styles: [
     `
       h1 {
         color: green;
       }
-    `
-  ]
+    `,
+  ],
 })
 export class AppComponent {
   title = 'POC Custom Element';
--- libs/chart2/src/app/app.module.ts
+++ libs/chart2/src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { createCustomElement } from '@angular/elements';
 
 import { AppComponent } from './app.component';
 
@@ -8,5 +9,14 @@ import { AppComponent } from './app.component';
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
+  entryComponents: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private injector: Injector) {
+    const el = createCustomElement(AppComponent, { injector });
+    customElements.define('chart-element', el);
+  }
+
+  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method, @typescript-eslint/no-empty-function, @angular-eslint/use-lifecycle-interface
+  ngDoBootstrap() {}
+}
